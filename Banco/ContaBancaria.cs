7ea1e6d4380a5d574e08c3b45899e70a6ad107db
Chris-gus <chrisgus2605@gmail.com>
2025-03-14 09:04:28 -0300

    Primeiro commit

---
+++ Banco/ContaBancaria.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    public class ContaBancaria
+    {
+        private int _numeroConta;
+        private string _titular;
+        private double _saldo;
+
+        public ContaBancaria(int numeroConta, string titular, double saldoInicial)
+        {
+            _numeroConta = numeroConta;
+            _titular = titular;
+            _saldo = saldoInicial;
+        }
+        public double Depositar
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                _saldo += value;
+            }
+        }
+        public double Sacar
+        {
+            get {
+                return _saldo; 
+            }
+
+            set
+            {
+                if(_saldo > value) 
+                {
+                    _saldo -= value;
+                }
+            }
+        }
+        public void ExibirSaldo() 
+        {
+            Console.WriteLine(_saldo);
+        }
+    }
+}
